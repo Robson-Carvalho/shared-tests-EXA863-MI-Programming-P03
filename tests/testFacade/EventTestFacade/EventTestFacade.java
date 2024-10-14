77034fb6ce3213d71e7befd9589f3aa44ac33057
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-14 14:46:57 -0300

    test: added event test facade

---
+++ tests/testFacade/EventTestFacade/EventTestFacade.java
@@ -0,0 +1,55 @@
+
+public class EventTestFacade {
+
+    public EventTestFacade() {
+        
+    }
+
+    public void removeSeatByEventId(String seat, String id){
+       
+    }
+
+    public void addSeatByEventId(String seat, String id){
+     
+    }
+
+    public String create(String loginAdmin, String name, String description, Date date){
+     
+    }
+
+    public Event getById(String id){
+
+    }
+
+    public String getNameByEventId(String id){
+    
+    }
+
+    public List<String> getSeatsByEventId(String id){
+
+    }
+
+    public String getDescriptionByEventId(String id){
+      
+    }
+
+    public int getYearByEventId(String id){
+      
+    }
+
+    public int getMonthByEventId(String id){
+      
+    }
+
+    public int getDayByEventId(String id){
+     
+    }
+
+    public boolean getIsActiveByEventId(String id){
+        
+    }
+
+    public void deleteAllEvents(){
+     
+    }
+}
