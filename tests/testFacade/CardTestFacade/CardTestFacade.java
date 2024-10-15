29060b028b683a330d1079fbdf563b2819d0f9dc
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-15 18:47:38 -0300

    test: added CardTestFacade

---
+++ tests/testFacade/CardTestFacade/CardTestFacade.java
@@ -0,0 +1,52 @@
+
+public class CardTestFacade {
+
+
+    public CardTestFacade() {
+
+    }
+
+    public String create(String userEmail, String cardNumber, Date expiryDate, int cvv){
+
+    }
+
+    public String getUserNameByCardId(String cardId){
+
+    }
+
+    public String getCardNumberByCardId(String cardId){
+
+    }
+
+    public int getYearByCardId(String cardId){
+
+    }
+
+    public int getMonthByCardId(String cardId){
+
+    }
+
+    public int getDayByCardId(String cardId){
+
+    }
+
+    public void disable(String cardId){
+
+    }
+
+    public boolean getStatusByCardId(String cardId){
+        
+    }
+
+    public void delete(String cardId){
+
+    }
+
+    public Card getById(String id){
+
+    }
+
+    public void deleteAllCards(){
+        
+    }
+}
