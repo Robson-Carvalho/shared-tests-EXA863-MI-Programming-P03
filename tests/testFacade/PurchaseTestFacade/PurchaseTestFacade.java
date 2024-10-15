d58c467b685d30b806084a543c14643379ce2596
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-15 18:50:02 -0300

    test: added PurchaseTestFacade

---
+++ tests/testFacade/PurchaseTestFacade/PurchaseTestFacade.java
@@ -0,0 +1,42 @@
+
+public class PurchaseTestFacade {
+
+    public PurchaseTestFacade() {
+
+    }
+
+    public String create(String email, String eventId, String seat){
+
+    }
+
+    public String create(String email, String eventId, String cardId, String seat){
+
+    }
+
+    public Transaction getById(String id){
+    }
+
+    public String getEventByPurchaseId(String id){
+        
+    }
+
+    public String getUserLoginByPurchaseId(String id){
+        
+    }
+
+    public Ticket getTicketByPurchaseId(String id){
+
+    }
+
+    public int getUserMailBoxByPurchaseId(String id){
+        
+    }
+
+    public Card getCardByPurchaseID(String id){
+        
+    }
+
+    public void deleteAllPurchases(){
+        
+    }
+}
