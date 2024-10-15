1013c38a6841b5890e7c7cd9db47491ac01fd9de
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-15 11:02:20 -0300

    test: added comment test

--- tests/unitary/CommentTest/CommentTest.java
+++ tests/unitary/CommentTest/CommentTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CommentTest {
     private UserTestFacade userTestFacade;
@@ -31,7 +30,7 @@ public class CommentTest {
     }
 
     @Test
-    public void createCommentTest() {
+    public void testCreateComment() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2020, Calendar.SEPTEMBER, 10);
         String name = "Show de Rock";
@@ -56,6 +55,40 @@ public class CommentTest {
         assertEquals(2, commentTestFacade.getRatingCommentById(commentId));
         assertEquals(userTestFacade.getUserIdByEmail(email), commentTestFacade.getUserIdById(commentId));
         assertEquals(eventId, commentTestFacade.getEventIdById(commentId));
+    }
+
+    @Test
+    public void testCreateCommentFutureEvent() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2025, Calendar.SEPTEMBER, 10);
+        String name = "Show de Rock";
+        String description = "Banda XYZ";
+        Date date = calendar.getTime();
+
+        String _login = "LS@";
+        String _nameUser = "Lia Silva";
+        String _email = "lia@example.com";
+        String _password = "teste123";
+        String _cpf = "987654321";
+        Boolean _isAdmin = true;
+
+        String login = "Lr@";
+        String nameUser = "Lara";
+        String email = "lr@example.com";
+        String password = "teste123";
+        String cpf = "123456789";
+        Boolean isAdmin = false;
+
+        userTestFacade.create(_login, _password, _nameUser, _cpf, _email, _isAdmin);
+        userTestFacade.create(login, password, nameUser, cpf, email, isAdmin);
+
+        String eventId = eventTestFacadeFacade.create(_login, name, description, date);
+
+        Exception exception = assertThrows(SecurityException.class, () -> {
+            commentTestFacade.create(userTestFacade.getUserIdByEmail(email), eventId, 4, "Bom evento!");
+        });
+
+        assertEquals("Comentário só pode ser adicionando após a realização do evento.", exception.getMessage());
     }
 
     @Test
@@ -87,5 +120,57 @@ public class CommentTest {
 
         assertEquals(1, eventTestFacadeFacade.getCommentQuantityByEventId(firstEventId));
         assertEquals(2, eventTestFacadeFacade.getCommentQuantityByEventId(secondEventId));
+    }
+
+
+    @Test
+    public void ratingAverageTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2020, Calendar.SEPTEMBER, 10);
+        Date date = calendar.getTime();
+
+        String login = "LS@";
+        String nameUser = "Lia Silva";
+        String email = "lia@example.com";
+        String password = "teste123";
+        String cpf = "987654321";
+        Boolean isAdmin = false;
+
+        userTestFacade.create(login, password, nameUser, cpf, email, isAdmin);
+
+        String eventId = eventTestFacadeFacade.addEventInDataBaseWithPastDate("Show de Rock", "Evento musical", date);
+
+        String c1Id = commentTestFacade.create(userTestFacade.getUserIdByEmail(email), eventId, 4, "Bom evento, mas pode melhorar!");
+        String c2Id = commentTestFacade.create(userTestFacade.getUserIdByEmail(email), eventId, 5, "Bom evento!");
+
+        assertNotNull(commentTestFacade.getById(c1Id));
+        assertNotNull(commentTestFacade.getById(c2Id));
+
+        assertEquals(4.5, commentTestFacade.getEventRatingByEventId(eventId), 0.001);
+    }
+
+    @Test
+    public void deleteCommentTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2020, Calendar.SEPTEMBER, 10);
+        Date date = calendar.getTime();
+
+        String login = "LS@";
+        String nameUser = "Lia Silva";
+        String email = "lia@example.com";
+        String password = "teste123";
+        String cpf = "987654321";
+        Boolean isAdmin = false;
+
+        userTestFacade.create(login, password, nameUser, cpf, email, isAdmin);
+
+        String eventId = eventTestFacadeFacade.addEventInDataBaseWithPastDate("Show de Rock", "Evento musical", date);
+
+        String c1Id = commentTestFacade.create(userTestFacade.getUserIdByEmail(email), eventId, 4, "Bom evento, mas pode melhorar!");
+        assertNotNull(commentTestFacade.getById(c1Id));
+
+        commentTestFacade.delete(c1Id);
+
+        assertNull(commentTestFacade.getById(c1Id));
     }
 }
